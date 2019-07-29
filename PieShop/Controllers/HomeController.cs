dcd3f549b625ac5940a8bdbd0f128745c3d821d7
Rodion Gyrbu <fpsoff@outlook.com>
2019-07-29 17:41:19 +0300

    mvc start

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Pie overview";
+
+            var pies = pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            return View();
+        }
+    }
+}
